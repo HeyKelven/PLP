ae5a65d2568332dd870516ce5aa89027743b8dd7
HeyKelven <victork.laerte@gmail.com>
2019-05-02 22:02:16 -0300

    matrix

--- matrix.c
+++ matrix.c
@@ -1,23 +1,22 @@
 #include<stdio.h>
 #include<mpi.h>
 
-int b [] = {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 
-1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 
-1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 
-1, 2, 1, 2, 1, 2, 1, 2, 1, 2};
+int b [][] = {{1, 1, 1, 1},{2, 2, 2, 2},{3, 3, 3, 3},{4, 4, 4, 4}};
+int w [] = {1, 2, 3, 4};
 
 
 int main(int argc, char **argv){
 
-    int size, id, length, dest = 0, tag = 999;
-    char name[80];
+    int size, id;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int i, s, z = 0, r = 40/size;
+    int j, i, s, z = 0, r = 40/size;
     
+    //B = A( 0 : n -1, id . r : (id + 1)r -1)
+    for
     for(i = id*r ; i <= ((id +1) *r) - 1; i ++){
         z += b[i];
     }
